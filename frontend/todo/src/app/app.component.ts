8be02ef49ca4a91b5e86c4f309e7056e5aa704ad
Suren k v <suren@jsworkbench.com>
2020-09-20 09:50:42 +0530

    Todo App Complete

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -1,32 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Color } from '@sz/enum';
 
 @Component({
   selector: 'sz-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit {
+  todoForm: FormGroup;
+  todoOptions = {
+    primaryAction: { icon: 'check' },
+    secondaryAction: { icon: 'clear', color: Color.ERROR },
+    heroBg: 'transparent',
+    reverse: true,
+    mainBg: '#EEEEEE',
+  };
+  mapped = false;
+  todoList = [
+    {
+      description: 'This is a Medium Task',
+      color: Color.GENTLE,
+    },
+    {
+      description: 'High Priority Task',
+      color: Color.ERROR,
+    },
+    {
+      description: 'Important Task',
+      color: Color.WARN,
+    },
+  ];
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.todoList.map(item => Object.assign(item, this.todoOptions));
+    this.mapped = true;
+    this.todoForm = this.fb.group({
+      description: [''],
+    });
+  }
+
+  primaryClick(e) {
+    this.todoList.splice(e, 1);
+  }
+  secondaryClick(e) {
+    this.todoList.splice(e, 1);
+  }
+
+  addTodo() {
+    let todo = this.todoForm.value;
+    if (this.todoForm.value.description) {
+      todo.color = Color.GENTLE;
+      todo = Object.assign(todo, this.todoOptions);
+      this.todoList.push(todo);
+      this.todoForm.reset();
+    }
+  }
 }
--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LibModule } from 'frontend/shared/src/lib/lib.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, LibModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
